73f4cf5ba23dab4e8694503be2ed19c1f5b58b11
SocketFinance <72254927+socket-finance@users.noreply.github.com>
2020-10-17 20:21:31 +0800

    add ETH/SFI pool

--- src/constants/stakePools.ts
+++ src/constants/stakePools.ts
@@ -3,9 +3,20 @@ import {
   WETH_TOKEN_ADDRESS,
   SFI_POOL_ADDRESS,
   SFI_TOKEN_ADDRESS,
+  ETH_SFI_UNI_LP_POOL_ADDRESS,
+  ETH_SFI_UNI_LP_TOKEN_ADDRESS,
 } from './tokenAddresses'
 
 export const STAKE_POOLS = [
+  {
+    id: 'ETHSFI',
+    logos: ["/images/eth.png", "/images/sfi.png"],
+    name: 'ETH-SFI LP Pool',
+    address: ETH_SFI_UNI_LP_POOL_ADDRESS,
+    tokenAddress: ETH_SFI_UNI_LP_TOKEN_ADDRESS,
+    tokenName: 'ETH-SFI LP',
+    poolRateUnit: 'SFI / week'
+  },
   {
     id: 'WETH',
     logos: ["/images/weth.svg"],
--- src/constants/tokenAddresses.ts
+++ src/constants/tokenAddresses.ts
@@ -1,9 +1,9 @@
 // Tokens
 export const SFI_TOKEN_ADDRESS = '0x1e15abf152067e9fe4a48bbf094a71f5bb16325d'
 export const WETH_TOKEN_ADDRESS = '0xc02aaa39b223fe8d0a0e5c4f27ead9083c756cc2'
-export const ETH_SFI_UNI_LP_TOKEN_ADDRESS = ''
+export const ETH_SFI_UNI_LP_TOKEN_ADDRESS = '0x3a5baea0c276e1917806521b4db4f23179f512c3'
 
 // Pools
 export const WETH_POOL_ADDRESS = '0x52fac8e7a139127a734a208fb288e5d3604ba0fd'
 export const SFI_POOL_ADDRESS = '0xf1145745ad0abfc7587cfe3b4d1c498be85446c7'
-export const ETH_SFI_UNI_LP_POOL_ADDRESS = ''
+export const ETH_SFI_UNI_LP_POOL_ADDRESS = '0x42177e8b737D172Fe58C7672dc0fc5b784Db62d7'
